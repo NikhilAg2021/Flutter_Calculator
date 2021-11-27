0b71290ad5880524edfb0d477ac71957f9f656f1
Nikhil Agarwal <nikhilagarwal.20.na@gmail.com>
2021-11-27 22:18:11 +0530

    Flutter Basics Project

--- FlutterBasics/my_app/lib/main.dart
+++ FlutterBasics/my_app/lib/main.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Text('Hey Ninjas!'),
+  home: Scaffold(
+    appBar: AppBar(
+      title: const Text('Flutter Basics'),
+      centerTitle: true,
+    ),
+    body: const Center(
+      child: Text('Hello World !!!'),
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: () {  },
+      child: const Text('Click'),
+    ),
+  ),
 ));
