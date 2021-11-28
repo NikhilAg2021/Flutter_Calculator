04927e90b7055784cc7849f26100ca27a7bd07e3
Nikhil Agarwal <nikhilagarwal.20.na@gmail.com>
2021-11-28 17:16:53 +0530

    Most Widgets of Flutter Completed

--- FlutterBasics/my_app/lib/main.dart
+++ FlutterBasics/my_app/lib/main.dart
@@ -1,17 +1,33 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: const Text('Flutter Basics'),
-      centerTitle: true,
-    ),
-    body: const Center(
-      child: Text('Hello World !!!'),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {  },
-      child: const Text('Click'),
-    ),
-  ),
+void main() => runApp(const MaterialApp(
+  home: Home(),
 ));
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Basics'),
+        centerTitle: true,
+        backgroundColor: Colors.amber[500],
+      ),
+      body: Center(
+        child: IconButton(
+          onPressed: () {
+            print("Icon Clicked ");
+          },
+          icon: Icon(Icons.mail),
+          color: Colors.amber,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {  },
+        child: const Text('Click'),
+        backgroundColor: Colors.red[300],
+      ),
+    );
+  }
+}
