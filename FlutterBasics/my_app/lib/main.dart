5b786661ba0e45567cd992b16440511b9b631eeb
Nikhil Agarwal <nikhilagarwal.20.na@gmail.com>
2021-11-29 22:59:30 +0530

    Working with the layouts in Flutter

--- FlutterBasics/my_app/lib/main.dart
+++ FlutterBasics/my_app/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MaterialApp(
+void main() => runApp(MaterialApp(
   home: Home(),
 ));
 
@@ -10,24 +10,55 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Flutter Basics'),
+        title: Text('Flutter Basics'),
         centerTitle: true,
         backgroundColor: Colors.amber[500],
       ),
-      body: Center(
-        child: IconButton(
-          onPressed: () {
-            print("Icon Clicked ");
-          },
-          icon: Icon(Icons.mail),
-          color: Colors.amber,
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text('Hello World pt.1'),
+          TextButton(
+            onPressed: () {},
+            child: Text('Click Me'),
+
+          ),
+            Container(
+              padding: EdgeInsets.all(20.0),
+              color: Colors.cyan[400],
+              child: Text('Hello World!!!'),
+      ),
+      ],
+          ),
+          Container(
+            padding: EdgeInsets.all(20.0),
+            color: Colors.red[400],
+            child: Text('One!!'),
+          ),
+          Container(
+            padding: EdgeInsets.all(20.0),
+            color: Colors.cyan[400],
+            child: Text('two!!!'),
+          ),
+          Container(
+            padding: EdgeInsets.all(20.0),
+            color: Colors.amber[400],
+            child: Text('Three!!!'),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {  },
-        child: const Text('Click'),
-        backgroundColor: Colors.red[300],
+        child: Text('Click'),
+        backgroundColor: Colors.red,
       ),
+
     );
   }
 }
